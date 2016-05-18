6e681205c91ccfa51a0521815e39bde1bde87815
lefevr_h <lefevr_h@epitech.eu>
2016-05-18 13:18:14 +0200

    [*] inlude stdio.h

--- include/42.h
+++ include/42.h
@@ -1,30 +1,32 @@
 /*
 ** 42.h for  in /home/xx/Rendu/PSU/42SH
-** 
+**
 ** Made by boris saint-bonnet
 ** Login   <saint-_o@epitech.net>
-** 
+**
 ** Started on  Wed May 18 04:20:11 2016 boris saint-bonnet
-** Last update Wed May 18 05:08:18 2016 boris saint-bonnet
+** Last update Wed May 18 13:18:12 2016 Philippe Lefevre
 */
 
-# ifndef __42_H__
-# define __42_H__
+#ifndef		__42_H__
+# define	__42_H__
 
 /*
 ** INCLUDES
 */
 
-# include <sys/types.h>
-# include <sys/stat.h>
-# include <fcntl.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include "prototype.h"
+# include	<stdio.h>
+# include	<string.h>
+# include	<sys/types.h>
+# include	<sys/stat.h>
+# include	<fcntl.h>
+# include	<stdlib.h>
+# include	<unistd.h>
+# include	"prototype.h"
 
 /*
 ** STRUCTURES
 */
 
 
-# endif /* !__42_H__ */ 
+#endif /* !__42_H__ */
