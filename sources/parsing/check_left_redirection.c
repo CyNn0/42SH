3319ad6bc2e5663e0ac19e0df7808ae8e56c575d
Lucas <lucas.gambini@epitech.eu>
2016-05-19 16:34:07 +0200

    [*] left redirection segv fixed

--- sources/parsing/check_left_redirection.c
+++ sources/parsing/check_left_redirection.c
@@ -1,20 +1,78 @@
 /*
 ** check_left_redirection.c for  in /home/xx/Rendu/PSU/42SH/sources
-** 
+**
 ** Made by boris saint-bonnet
 ** Login   <saint-_o@epitech.net>
-** 
+**
 ** Started on  Wed May 18 04:47:33 2016 boris saint-bonnet
-** Last update Wed May 18 06:39:49 2016 boris saint-bonnet
+** Last update Thu May 19 16:32:53 2016 Lucas Gambini
 */
 
 # include "42.h"
 
+char	*dep_left_redirection4(char *ret, int i)
+{
+  char	*bef;
+  char	*aft;
+
+  bef = my_strcut(ret, 0, (i + 5), 0);
+  aft = my_strcut(ret, (i + 5), 0, 0);
+  ret = my_strcatc(bef, aft);
+  free(bef);
+  free(aft);
+  return (ret);
+}
+
+char	*dep_left_redirection3(char *ret, int i)
+{
+  char	*bef;
+  char	*aft;
+
+  bef = my_strcut(ret, 0, (i + 2), 0);
+  aft = my_strcut(ret, (i + 2), 0, 0);
+  ret = my_strcatc(bef, aft);
+  free(bef);
+  free(aft);
+  return (ret);
+}
+
+char	*dep_left_redirection2(char *s, char *ret, int i)
+{
+  char  *bef;
+  char  *aft;
+
+  if (ret != NULL)
+    {
+      bef = my_strcut(ret, 0, (i + 2), 0);
+      aft = my_strcut(ret, (i + 2), 0, 0);
+    }
+  else
+    {
+      bef = my_strcut(s, 0, (i + 1), 0);
+      aft = my_strcut(s, (i + 1), 0, 0);
+    }
+  ret = my_strcatc(bef, aft);
+  free(bef);
+  free(aft);
+  return (ret);
+}
+
+char	*dep_left_redirection1(char *s, char *ret, int i)
+{
+  char	*bef;
+  char	*aft;
+
+  bef = my_strcut(s, 0, i, 0);
+  aft = my_strcut(s, i, 0, 0);
+  ret = my_strcatc(bef, aft);
+  free(bef);
+  free(aft);
+  return (ret);
+}
+
 char    *check_left_redirection(char *s)
 {
   int   i;
-  char  *bef;
-  char  *aft;
   char  *ret;
 
   i = -1;
@@ -22,29 +80,13 @@ char    *check_left_redirection(char *s)
   while (s[++i])
     {
       if (s[i] == '<' && s[i - 1] != '<' && s[i + 1] != '<' && s[i - 1] != ' ')
-	{
-	  bef = my_strcut(s, 0, i, 0);
-	  aft = my_strcut(s, i, 0, 0);
-	  ret = my_strcatc(bef, aft);
-	}
+	ret = dep_left_redirection1(s, ret, i);
       if (s[i] == '<' && s[i - 1] != '<' && s[i + 1] != '<' && s[i + 1] != ' ')
-	{
-	  bef = my_strcut(ret, 0, (i + 2), 0);
-	  aft = my_strcut(ret, (i + 3), 0, 0);
-	  ret = my_strcatc(bef, aft);
-	}
+	ret = dep_left_redirection2(s, ret, i);
       if (s[i] == '<' && s[i + 1] == '<' && s[i - 1] != ' ')
-	{
-	  bef = my_strcut(ret, 0, (i + 2), 0);
-	  aft = my_strcut(ret, (i + 3), 0, 0);
-	  ret = my_strcatc(bef, aft);
-	}
+	ret = dep_left_redirection3(ret, i);
       if (s[i] == '<' && s[i + 1] == '<')
-	{
-	  bef = my_strcut(ret, 0, (i + 5), 0);
-	  aft = my_strcut(ret, (i + 5), 0, 0);
-	  ret = my_strcatc(bef, aft);
-	}
+	ret = dep_left_redirection4(ret, i);
     }
   if (ret == NULL)
     return (s);
