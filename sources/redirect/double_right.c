7f9a956637566d63966f0350f913fcda46e14d01
saint-_o <boris.saint-bonnet@epitech.eu>
2016-05-23 19:07:46 +0200

    Correction

--- sources/redirect/double_right.c
+++ sources/redirect/double_right.c
@@ -5,7 +5,7 @@
 ** Login   <saint-_o@epitech.net>
 ** 
 ** Started on  Mon May 23 18:34:11 2016 boris saint-bonnet
-** Last update Mon May 23 18:42:44 2016 boris saint-bonnet
+** Last update Mon May 23 19:07:09 2016 boris saint-bonnet
 */
 
 # include "42.h"
@@ -13,12 +13,13 @@
 int		double_right(t_cmd *cmd, t_path *path, char **env)
 {
   t_right       var;
-
+  int		fd;
+  
   var = parsing_sr(cmd->cmd);
-  if ((fd = open(__DOUBLE)) == -1)
+  if ((fd = open(DOUBLE)) == -1)
     return (FAILURE);
   dup2(fd, 1);
   simple_exec(cmd, path, env);
   dup2(fd, 1);
-  return (SUCCES);
+  return (SUCCESS);
 }
--- sources/redirect/simple_right.c
+++ sources/redirect/simple_right.c
@@ -5,7 +5,7 @@
 ** Login   <saint-_o@epitech.net>
 ** 
 ** Started on  Mon May 23 17:57:28 2016 boris saint-bonnet
-** Last update Mon May 23 18:49:41 2016 boris saint-bonnet
+** Last update Mon May 23 19:07:27 2016 boris saint-bonnet
 */
 
 # include "42.h"
@@ -13,7 +13,8 @@
 int		simple_right(t_cmd *cmd, t_path *path, char **env)
 {
   t_right	var;
-
+  int		fd;
+  
   var = parsing_sr(cmd->cmd);
   if ((fd = open(__SIMPLE)) == -1)
     return (FAILURE);
@@ -21,5 +22,5 @@ int		simple_right(t_cmd *cmd, t_path *path, char **env)
   son = fork();
   simple_exec(cmd, path, env);
   dup2(1, fd);
-  return (SUCCES);
+  return (SUCCESS);
 }
