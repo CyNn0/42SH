bf6f7230121289dfd34cd1eef35a4789332656fe
Lucas <lucas.gambini@epitech.eu>
2016-05-23 18:42:34 +0200

    [*] scatter.c

--- sources/parsing/scatter.c
+++ sources/parsing/scatter.c
@@ -5,7 +5,7 @@
 ** Login   <gambin_l@epitech.net>
 **
 ** Started on  Mon May 23 16:43:07 2016 Lucas Gambini
-** Last update Mon May 23 18:21:37 2016 Gambini Lucas
+** Last update Mon May 23 18:40:35 2016 Gambini Lucas
 */
 
 #include "42.h"
@@ -13,8 +13,19 @@
 int	exec_scatter(t_list *list)
 {
   char	**env;
+  t_cmd	*tmp;
 
+  tmp = list->head;
   env = extract_env(list->myEnv);
-  show_tab(env);
+  (void)env;
+  while (tmp)
+    {
+      (tmp->flag == SIMPLE_R) ? (printf("SIMPLE_R\n"))
+	  : ((tmp->flag == SIMPLE_L) ? (printf("SIMPLE_L\n"))
+	      : ((tmp->flag == DOUBLE_R) ? (printf("DOUBLE_R\n"))
+		  : ((tmp->flag == DOUBLE_L) ? (printf("DOUBLE_L\n"))
+		      : ((printf("EXEC SIMPLE\n"))))));
+      tmp = tmp->next;
+    }
   return (SUCCESS);
 }
