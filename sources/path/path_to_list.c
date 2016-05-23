41068b4c437120ea53eb679d66245e417096b188
Lucas <lucas.gambini@epitech.eu>
2016-05-23 11:16:53 +0200

    [*] env -u PATH && env -i SEGV fixed

--- sources/path/path_to_list.c
+++ sources/path/path_to_list.c
@@ -1,11 +1,11 @@
 /*
 ** path_to_list.c for  in /home/xx/Rendu/PSU/42SH
-** 
+**
 ** Made by boris saint-bonnet
 ** Login   <saint-_o@epitech.net>
-** 
+**
 ** Started on  Thu May 19 23:48:19 2016 boris saint-bonnet
-** Last update Fri May 20 04:24:34 2016 boris saint-bonnet
+** Last update Mon May 23 11:14:48 2016 Lucas Gambini
 */
 
 # include "42.h"
@@ -18,17 +18,23 @@ int     path_nb(char *path)
   i = -1;
   nb = 0;
   while (path && path[++i])
-    if (path[i] == ':')
-      nb++;
+    {
+      if (path[i] == ':')
+	nb++;
+    }
   return (nb + 1);
 }
 
 t_list  *path_to_list(t_list *list)
 {
   char          *var;
+  char		*path_line;
   int           i;
 
-  var = strdup(find_path(list));
+  if ((path_line = find_path(list)) != NULL)
+    var = strdup(path_line);
+  else
+    return (list);
   i = path_nb(var);
   if ((list = push_path(list, my_strtok(var, ":"))) == NULL)
     return (NULL);
