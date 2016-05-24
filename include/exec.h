231c5d390ff206d99eeb87f65eb0fba45584de63
Lucas <lucas.gambini@epitech.eu>
2016-05-24 09:15:40 +0200

    [*] Header corrigé

--- include/exec.h
+++ include/exec.h
@@ -1,11 +1,11 @@
 /*
-** exec.h for exec in C:\Users\gambin_l\Documents\Virtual Machines\Shared\42SH\include
+** exec.h for exec in C:\Users\gambin_l\Shared\42SH\include
 **
 ** Made by Gambini Lucas
 ** Login   <Lucas Gambini@epitech.net>
 **
 ** Started on  Mon May 23 19:17:09 2016 Gambini Lucas
-** Last update Mon May 23 23:36:30 2016 Philippe Lefevre
+** Last update Tue May 24 09:14:17 2016 Gambini Lucas
 */
 
 #ifndef			_EXEC_H_
