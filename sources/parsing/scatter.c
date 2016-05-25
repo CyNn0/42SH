9568bb16008c78515ec53e13b2a4661fc7cfc003
lefevr_h <lefevr_h@epitech.eu>
2016-05-25 11:42:52 +0200

    [*] exit +

--- sources/parsing/scatter.c
+++ sources/parsing/scatter.c
@@ -5,7 +5,7 @@
 ** Login   <gambin_l@epitech.net>
 **
 ** Started on  Mon May 23 16:43:07 2016 Lucas Gambini
-** Last update Wed May 25 11:19:21 2016 Philippe Lefevre
+** Last update Wed May 25 11:42:17 2016 Philippe Lefevre
 */
 
 #include	"42.h"
@@ -18,15 +18,10 @@ int		exec_scatter(t_list *list)
 
   tmp = list->head;
   env = extract_env(list->myEnv);
-  while (tmp && tmp->cmd[0])
+  while (!(list->do_exit) && tmp && tmp->cmd[0])
     {
       if ((builtin = check_built(tmp)) == FAILURE && tmp->go_on == 1)
 	normal_scatter(tmp, env, list->path);
-      if (builtin == 24)
-	{
-	  free_tab(env);
-	  return (24);
-	}
       /*else
 	builtin_scatter(list, tmp, builtin);*/
       tmp = tmp->next;
