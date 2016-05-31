5df94cf9dfa0f2decd475ec61ee4ae276f92d92b
lefevr_h <lefevr_h@epitech.eu>
2016-05-31 22:24:24 +0200

    [*] PIPE INF MOTHER FUCKER

--- sources/pipe/pipe.c
+++ sources/pipe/pipe.c
@@ -5,7 +5,7 @@
 ** Login   <lefevr_h@epitech.net>
 **
 ** Started on  Mon May 23 19:04:26 2016 Philippe Lefevre
-** Last update Tue May 31 22:02:51 2016 Philippe Lefevre
+** Last update Tue May 31 22:23:52 2016 Philippe Lefevre
 */
 
 #include		"42.h"
@@ -21,7 +21,7 @@ int			prepare_pipe(t_cmd *cmd, t_list *list)
   out = 0;
   while (tmp)
     {
-      if (tmp->token == PIPE)
+      if (tmp->token == PIPE || (tmp->prev && tmp->prev->token == PIPE))
 	{
 	  if ((out == 0) && (tmp->cmd[0] =
 	       exec_find_path(list->path, tmp->cmd[0])) == NULL)
@@ -29,6 +29,7 @@ int			prepare_pipe(t_cmd *cmd, t_list *list)
 	      fprintf(stderr, "%s: Command not found.\n", tmp->cmd[0]);
 	      out = -1;
 	    }
+	  printf("==[%s]\n", tmp->cmd[0]);
 	  count_pipe += 1;
 	}
       tmp = tmp->next;
@@ -43,18 +44,20 @@ int			exec_child(t_cmd *cmd, t_list *list, char **env,
 {
   pid_t			pid;
 
-  if (pipe(cmd->fd) == -1)
-    fprintf(stderr, "Error: %s\n", strerror(errno));
-  if ((pid = fork()) == -1)
-    fprintf(stderr, "Error: %s\n", strerror(errno));
+  if ((pipe(cmd->fd) == -1)
+      || ((pid = fork()) == -1))
+    {
+      fprintf(stderr, "Error: %s\n", strerror(errno));
+      exit(FAILURE);
+    }
   else if (pid == 0)
     {
-      if (cmd->token != PIPE)
-	close(cmd->fd[1]);
-      else
+      if (cmd->token == PIPE)
 	dup2(cmd->fd[1], 1);
-      normal_scatter(cmd, env, list, builtin - 20);
-      my_exit(0);
+      if (builtin >= 0)
+	exit(simple_exec_builtin(list, cmd, builtin));
+      execve(cmd->cmd[0], cmd->cmd, env);
+      fprintf(stderr, "Error: %s %s\n", cmd->cmd[0], strerror(errno));
     }
   dup2(cmd->fd[0], 0);
   return (SUCCESS);
@@ -71,11 +74,11 @@ int			exec_pipe(t_cmd *cmd, t_list *list, char **env,
   if ((count_pipe = prepare_pipe(cmd, list)) == -1)
     return (FAILURE);
   back_stdin = dup(0);
-  cur = -1;
+  cur = 0;
   tmp = cmd;
   while (++cur < count_pipe)
     {
-      exec_child(tmp, list, env, builtin);
+      exec_child(tmp, list, env, (builtin - 20));
       tmp->go_on = 0;
       tmp = tmp->next;
     }
