5f09aa04e918eb1c9ac3bcf90e2d2e3cb37728c7
Lucas <lucas.gambini@epitech.eu>
2016-05-31 10:12:13 +0200

    [+] norme double_left

--- sources/redirect/double_left.c
+++ sources/redirect/double_left.c
@@ -5,7 +5,7 @@
 ** Login   <Lucas Gambini@epitech.net>
 **
 ** Started on  Mon May 30 10:56:47 2016 Gambini Lucas
-** Last update Tue May 31 10:06:53 2016 Gambini Lucas
+** Last update Tue May 31 10:11:52 2016 Gambini Lucas
 */
 
 #include 		"42.h"
@@ -32,34 +32,44 @@ char    *my_concate(char *base, char *add)
   return (ret);
 }
 
-int             	double_left(t_cmd *cmd, t_list *list,
-				    char **env, int builtin)
+char			*get_buffer(t_red var)
 {
-  t_red       		var;
   char			*buff;
   char			*res;
-  int			reset;
 
   res = NULL;
-  reset = dup(0);
-  init_double_left(cmd->cmd, &var);
-  var.is_builtin = builtin;
   while (write(1, "? ", 2) && (buff = get_next_line(0)))
     {
       if (strcmp(buff, var.name) == 0)
 	break;
       else
-	if (!res)
-	  res = strdup(buff);
-	else
-	  res = my_concate(res, buff);
+	{
+	  if (!res)
+	    res = strdup(buff);
+	  else
+	    res = my_concate(res, buff);
+	}
       free(buff);
     }
-    /* CA MARCHE PAS ! Go écrire dans un tmp ?? */ dup2(1, 0);
-  printf("%s\n", res);
-    if (exec_left(list, env, var) == FAILURE)
-      check_go_on(cmd);
-    dup2(reset, 0);
+  return (res);
+}
+
+int             	double_left(t_cmd *cmd, t_list *list,
+				    char **env, int builtin)
+{
+  t_red       		var;
+  int			reset;
+  char			*buff;
+
+  init_double_left(cmd->cmd, &var);
+  var.is_builtin = builtin;
+  buff = get_buffer(var);
+  reset = dup(0);
+  /* CA MARCHE PAS ! Go écrire dans un tmp ?? */ dup2(1, 0);
+  printf("%s\n", buff);
+  if (exec_left(list, env, var) == FAILURE)
+    check_go_on(cmd);
+  dup2(reset, 0);
   free(var.name);
   free_tab(var.cmd);
   return (SUCCESS);
