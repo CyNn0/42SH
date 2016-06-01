c14d38b3697abc36485ac40cc4ae644ce86987db
Lucas <lucas.gambini@epitech.eu>
2016-06-01 17:53:20 +0200

    [+] doubles gauches op clean

--- sources/redirect/double_left.c
+++ sources/redirect/double_left.c
@@ -5,7 +5,7 @@
 ** Login   <Lucas Gambini@epitech.net>
 **
 ** Started on  Mon May 30 10:56:47 2016 Gambini Lucas
-** Last update Wed Jun 01 16:30:27 2016 Gambini Lucas
+** Last update Wed Jun 01 17:51:40 2016 Gambini Lucas
 */
 
 #include 		"42.h"
@@ -53,66 +53,30 @@ char			*get_buffer(t_red var)
   return (res);
 }
 
-int			open_file(char *buff)
-{
-  int			fd;
-
-  if ((fd = open(".tmp", O_RDWR | O_CREAT | O_TRUNC, __RIGHT)) == -1)
-    return (fprintf(stderr, "Error: %s: %s\n", strerror(errno),
-		    ".tmp") * 0 + FAILURE);
-    if (buff)
-      write(fd, buff, strlen(buff));
-    else
-      return (FAILURE);
-    close(fd);
-    if ((fd = open(".tmp", O_RDONLY)) == -1)
-      return (fprintf(stderr, "Error: %s: %s\n", strerror(errno),
-		      ".tmp") * 0 + FAILURE);
-      return (fd);
-}
-
-int			delete_tmp(char **env)
-{
-    char		*del[3];
-    int			pid;
-
-    del[0] = "/bin/rm";
-    del[1] = ".tmp";
-    del[2] = NULL;
-    if ((pid = fork()) == -1)
-      fprintf(stderr, "Error: Fork Failed\n");
-    else if (pid == 0)
-      {
-        execve(del[0], del, env);
-        my_exit(-1);
-      }
-    else
-      xwaitpid(pid, 0, 0);
-    return (SUCCESS);
-}
-
 int             	double_left(t_cmd *cmd, t_list *list,
 				    char **env, int builtin)
 {
   t_red       		var;
   char			*buff;
-  int			fd;
+  int			pipefd[2];
   int			reset;
 
+  pipe(pipefd);
   init_double_left(cmd->cmd, &var);
   var.is_builtin = builtin;
   if ((buff = get_buffer(var)) == NULL)
-      return (FAILURE);
-    if ((fd = open_file(buff)) == -1)
-      return (FAILURE);
+    return (FAILURE);
   reset = dup(0);
-  dup2(fd, 0);
-    show_tab(var.cmd);
+  if (buff)
+    write(pipefd[1], buff, strlen(buff));
+  else
+    return (FAILURE);
+  close(pipefd[1]);
+  dup2(pipefd[0], 0);
   if (exec_left(list, env, var) == FAILURE)
     check_go_on(cmd);
+  close(pipefd[0]);
   dup2(reset, 0);
-  close(fd);
-  delete_tmp(env);
   free(var.name);
   free_tab(var.cmd);
   return (SUCCESS);
