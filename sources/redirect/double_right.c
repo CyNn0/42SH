a9e193cb7e0317775e0c908b8a38bcffad9702ef
lefevr_h <lefevr_h@epitech.eu>
2016-06-03 14:38:40 +0200

    [*] quelque message derreur

--- sources/redirect/double_right.c
+++ sources/redirect/double_right.c
@@ -5,7 +5,7 @@
 ** Login   <saint-_o@epitech.net>
 **
 ** Started on  Mon May 23 18:34:11 2016 boris saint-bonnet
-** Last update Mon May 30 10:58:19 2016 Gambini Lucas
+** Last update Fri Jun 03 14:36:20 2016 Philippe Lefevre
 */
 
 # include 	"42.h"
@@ -18,7 +18,7 @@ int             double_right(t_cmd *cmd, t_list *list, char **env, int builtin)
   init_double(cmd->cmd, &var);
   var.is_builtin = builtin;
   if ((fd = open(var.name, __DOUBLE)) == -1)
-    return (FAILURE);
+    return (fprintf(stderr, "%s: %s\n", var.name, strerror(errno)) * 0 + FAILURE);
   exec_right(fd, list, env, var);
   close(fd);
   free(var.name);
