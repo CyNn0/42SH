7b3af2b8f7121b58bd909d0690314596988873ec
lefevr_h <lefevr_h@epitech.eu>
2016-06-04 16:31:34 +0200

    [*] bug fix bultin

--- sources/builtins/env.c
+++ sources/builtins/env.c
@@ -5,7 +5,7 @@
 ** Login   <lefevr_h@epitech.net>
 **
 ** Started on  Sat Jun 04 15:21:47 2016 Philippe Lefevre
-** Last update Sat Jun 04 15:36:17 2016 Philippe Lefevre
+** Last update Sat Jun 04 16:13:45 2016 Philippe Lefevre
 */
 
 #include		"42.h"
@@ -13,14 +13,22 @@
 int			builtin_env(t_list *list, char **cmd)
 {
   t_node		*tmp;
+  int			i;
+  int			no_line;
 
+  i = 0;
+  no_line = 0;
+  while (cmd[++i])
+    if ((!strcmp("-0", cmd[i]))
+	|| (!strcmp("--null", cmd[i])))
+	no_line = 1;
   tmp = list->myEnv->head;
   if (list != NULL)
     {
       while (tmp != NULL)
 	{
 	  if (!(tmp->god))
-	    printf("%s=%s\n", tmp->name, tmp->data);
+	    printf("%s=%s%c", tmp->name, tmp->data, ((no_line) ? (0) : ('\n')));
 	  tmp = tmp->next;
 	}
     }
